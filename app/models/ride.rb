a393b521097065c2fe585e054df164a3033e2e6f
Michael Ramuta <michaelramuta@gmail.com>
2017-08-31 21:29:17 -0500

    remove format_distance

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,13 +2,6 @@ require 'net/http'
 require 'json'
 
 class Ride < ApplicationRecord
-	def self.format_distance(ride, distance=nil)
-		if distance.nil?
-			distance = Ride.where(start_station_id: ride.start_station_id, end_station_id: ride.end_station_id).first_or_initialize.distance
-		end
-
-		{distance: distance, start_station_id: ride.start_station_id, end_station_id: ride.end_station_id}
-	end
 
 	def self.calculate_ride(ride)
 		start_station = Station.find_by(station_id: ride.start_station_id)
